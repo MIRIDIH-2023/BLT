23dcfa466dc92dfd32473e2e3d35a7e377a5b376
hyeonjin <rufjekdi.65@gmail.com>
2023-11-16 12:10:46 +0000

    refactoring split_folder.py

--- utils/split_folders.py
+++ utils/split_folders.py
@@ -4,32 +4,64 @@ import shutil
 import os
 import pickle
 import json
-import shutil
 
-input_path = "/home/work/increased_en_data/BLT/posData"
-output_path = "/home/work/increased_en_data/BLT/data2"
+source_path = "/home/work/increased_en_data/BLT/all_data"
+renderPos_data_path = "/home/work/increased_en_data/BLT/posData"
+splitted_data_path = "/home/work/increased_en_data/BLT/data2"
+no_renderPos_data_list = []
 
-# files = os.listdir(input_path)
-# for file_ in tqdm(files) :
-#     source_file = os.path.join(input_path, file_)
-#     with open(source_file, 'rb') as f:
-#         try:
-#             obj = pickle.load(f)
-#             json_data = json.loads(json.dumps(obj, default=str))
-#         except:
-#             raise AssertionError(f"Wrong file: {source_file}")
-#     if(not json_data["no_RendorPos"]) :
-#         shutil.copy(source_file, output_path)
-#     else :
-#         print(file_)
+assert os.path.exists(source_path)
+assert os.path.exists(renderPos_data_path) and len(os.listdir(renderPos_data_path)) == 0
 
-# tqdm(splitfolders.ratio(input_path, output=output_path, seed=77, ratio=(0.8, 0.1, 0.1)))
+# /renderPos_data_path/json_data/ 경로에 데이터를 복사 붙여넣기 함
+target_path = os.path.join(renderPos_data_path, "json_data")
+os.makedirs(target_path, exist_ok=True)
 
+assert os.path.exists(splitted_data_path) and len(os.listdir(splitted_data_path)) == 0
 
-# count dir files
-# import os
+# TODO split하는 함수 정리하기
+# renderPos data만 따로 저장하는 로직
+files = os.listdir(source_path)
+assert len(files) != 0
+for file_ in tqdm(files) :
+    source_file = os.path.join(source_path, file_)
+    with open(source_file, 'rb') as f:
+        try:
+            obj = pickle.load(f)
+            json_data = json.loads(json.dumps(obj, default=str))
+        except:
+            raise AssertionError(f"Wrong file: {source_file}")
+    if(not json_data["no_RendorPos"]) :
+        shutil.copy(source_file, target_path)
+    else :
+        no_renderPos_data_list.append(file_)
 
-print("test: ", len(os.listdir("/home/work/increased_en_data/BLT/data2/test/json_data")))
-print("val: ",len(os.listdir("/home/work/increased_en_data/BLT/data2/val/json_data")))
-print("train: ",len(os.listdir("/home/work/increased_en_data/BLT/data2/train/json_data")))
-# print("data len: ", len(os.listdir("/home/work/increased_en_data/BLT/posData/json_data")))
+# train, eval, test 폴더를 생성해 8:1:1로 나눠서 분리하는 함수
+# input 폴더 조건
+'''
+/renderPos_data_path
+        |___ json_data
+'''
+# output 폴더 결과
+'''
+/splitted_data_path
+        |___train
+                |___ json_data
+        |___eval
+                |___ json_data
+        |___test
+                |___ json_data
+'''
+tqdm(splitfolders.ratio(renderPos_data_path, output=splitted_data_path, seed=77, ratio=(0.8, 0.1, 0.1)))
+
+data = { "no_render_pos" : no_renderPos_data_list }
+
+file_path = os.path.join(splitted_data_path, "no_renderPos_data_file_list.json")
+with open(file_path, 'w', encoding='utf-8') as file:
+    json.dump(data, file, indent="\t")
+
+# count dir files 
+print("test: ", len(os.listdir(os.path.join(splitted_data_path, "test", "json_data"))))
+print("val: ",len(os.listdir(os.path.join(splitted_data_path, "val", "json_data"))))
+print("train: ",len(os.listdir(os.path.join(splitted_data_path, "train", "json_data"))))
+print("total data: ", len(os.listdir(os.path.join(renderPos_data_path, "json_data"))))
